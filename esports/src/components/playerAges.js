53841eac87934e1731e3767e659dd15d79e20835
Robert Loquinario <lokinerio@gmail.com>
2019-06-09 16:38:58 -0700

    think i merged

--- esports/src/components/playerAges.js
+++ esports/src/components/playerAges.js
@@ -2,46 +2,37 @@ import React from "react";
 import { Bar } from "react-chartjs-2";
 
 class PlayerAges extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      chartData: {
-        type: "horizontalBar",
-        labels: ["<20", "20-22", "23-24", "25-28", ">28"],
-        datasets: [
-          {
-            label: ["Age Distribution of League of Legened Pro Players"],
-            data: [23, 22, 23, 33, 13],
-            backgroundColor: [
-              "rgba(54, 162, 235, 0.6)",
-              "rgba(255, 206, 86, 0.6)",
-              "rgba(75, 192, 192, 0.6)",
-              "rgba(153, 102, 255, 0.6)",
-              "rgba(255, 159, 64, 0.6)",
-              "rgba(255, 99, 132, 0.6)"
-            ]
-          }
-        ]
-      }
-    };
+  constructor(props) {
+    super(props);
+
+    this.updateD3(props);
   }
+
+  componentWillReceiveProps(newProps) {
+    this.updateD3(newProps);
+  }
+
+  updateD3(props) {}
 
   render() {
     console.log("in playerAges");
-
-    return (
-      <div className="App containter">
-        <Bar
-          data={this.state.chartData}
-          options={{
-            legend: {
-              display: true
-            },
-            maintainAspectRatio: false
-          }}
-        />{" "}
-      </div>
-    );
+    if (!this.props.data) {
+      return null;
+    } else {
+      return (
+        <div className="App containter">
+          <Bar
+            data={this.state.chartData}
+            options={{
+              legend: {
+                display: true
+              },
+              maintainAspectRatio: false
+            }}
+          />{" "}
+        </div>
+      );
+    }
   }
 }
 
