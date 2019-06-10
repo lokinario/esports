7d6ed855e91b35a29ce1d98e5fbb3e1d2fb27421
Kevin Lim <klim560@gmail.com>
2019-06-09 18:44:51 -0700

    updated winning player chart

--- esports/src/components/WinningPlayers.js
+++ esports/src/components/WinningPlayers.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { Pie } from "react-chartjs-2";
+import { Doughnut } from "react-chartjs-2";
+import 'chart.piecelabel.js';
+
 class WinningPlayers extends React.Component {
   constructor() {
     super();
     this.state = {
-      labels : ['United States','Republic of Korea','China','Sweden','Germany','Russian Federation','France','Denmark','Canada','United Kingdom','Brazil','Combined Others'],
+      labels : ['United States','Republic of Korea','China','Sweden','Germany','Russian Federation','France','Denmark','Canada','United Kingdom','Brazil','Smaller Countries Combined'],
       datasets:[{
         data:[2076,1521,778,772,664,478,370,330,325,248,242,1975],
         backgroundColor:["#FC345C","#8EA5EB","#A3DE83","#FFDD00","#af50af","#bb9c2a","#E84396","#2bd3e6","#89ac9e","#f6b28a","#b7ca7d","#07b240","#a33548"]
@@ -18,23 +20,35 @@ class WinningPlayers extends React.Component {
     return (
       <div className="containter">
       <h1>Percentage of Winning Players by Country</h1>
-      <Pie
+      <Doughnut
         data={{
           labels:this.state.labels,
           datasets:this.state.datasets
         }}
         options={{
+          pieceLabel: {
+            render: 'percentage',
+            fontColor: 'black',
+            fontSize: 14,
+            fontStyle: 'bold',
+            precision: 2
+          },
           tooltips:{
-            callbacks:{
-              label: function(tooltipItem,data) {
-                let value = data.datasets[0].data[tooltipItem.index]
-                return commaFormat(value);
+            callbacks: {
+              // this callback is used to create the tooltip label
+              label: function(tooltipItem, data) {
+                // get the data label and data value to display
+                // convert the data value to local string so it uses a comma seperated number
+                var dataLabel = data.labels[tooltipItem.index];
+                // add the currency symbol $ to the label
+                var winnings = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+                var value = ': ' + commaFormat(winnings);
+                dataLabel += value;
+                // return the text to display on the tooltip
+                return dataLabel;
               },
-              title: function(tooltipItem,data){
-                return data.labels[tooltipItem[0].index];
-              }
-            },
-            bodyFontSize:16
+              bodyFontSize:16
+            }
           }
         }}
       />
