29cdcdcbef69547b2466275a159968fd85499a60
twong40 <twong40@ucsc.edu>
2019-06-09 20:50:24 -0700

    bubble chart has the data

--- esports/src/components/Prize.js
+++ esports/src/components/Prize.js
@@ -9,14 +9,110 @@ class Prize extends React.Component {
         rs:null,
       datasets:[
         {
-         label: ["China"],
-         backgroundColor: "#FC345C",
-         borderColor: "#FC345C",
+         label: ["Fortnite"],
+         backgroundColor: "rgba(252, 52, 92, 0.6)",
+         borderColor: "rgba(252, 52, 92, 0.6)",
          data: [
-           {x: 39,y:7,r: 15},{x: 15,y: 5,r: 15}
+           {x:20055832.14	,r:174/2,y:1442}
         ]
-       }
-      ]
+        },
+         {
+          label: ["Dota 2"],
+          backgroundColor: "rgba(142, 165, 235, 0.6)",
+          borderColor: "rgba(142, 165, 235, 0.6)",
+          data: [
+            {x:41420078.99,r:167/2,y:1118}
+         ]
+       },
+       {
+        label: ["LoL"],
+        backgroundColor: "rgba(255, 221, 0, 0.6)",
+        borderColor: "rgba(255, 221, 0, 0.6)",
+          data: [
+            {x:14512216.23,r:178/2,y:1799}
+         ]
+       },
+       {
+        label: ["Overwatch"],
+        backgroundColor: "rgba(163, 222, 131, 0.6)",
+        borderColor: "rgba(163, 222, 131, 0.6)",
+          data: [
+            {x:6590091.65,r:95/2,y:1452}
+         ]
+       },
+       {
+        label: ["CS:GO"],
+        backgroundColor: "rgba(175, 80, 175, 0.6)",
+        borderColor: "rgba(175, 80, 175, 1)",
+          data: [
+            {x:22657892.39,r:828/2,y:4548}
+         ]
+       },
+       {
+        label: ["Rainbow Six Siege"],
+        backgroundColor: "rgba(187, 156, 42, 0.6)",
+        borderColor: "rgba(187, 156, 42, 1)",
+          data: [
+            {x:1884653.23,r:44/2,y:401}
+         ]
+       },
+       {
+        label: ["PUBG"],
+        backgroundColor: "rgba(232, 67, 150, 0.6)",
+        borderColor: "rgba(232, 67, 150, 1)",
+          data: [
+            {x:7572167.16,r:107/2,y:1473}
+         ]
+       },
+       {
+        label: ["Hearthstone"],
+        backgroundColor: "rgba(43, 211, 230, 0.6)",
+        borderColor: "rgba(43, 211, 230, 1)",
+          data: [
+            {x:4711852.27,r:131/2,y:828}
+         ]
+       },
+       {
+        label: ["Heroes of the Storm"],
+        backgroundColor: "rgba(137, 172, 158, 0.6)",
+        borderColor: "rgba(137, 172, 158, 1)",
+          data: [
+            {x:5277279.76,r:34/2,y:368}
+         ]
+       },
+       {
+        label: ["Starcraft II"],
+        backgroundColor: "rgba(246, 178, 138, 0.6)",
+        borderColor: "rgba(246, 178, 138, 1)",
+          data: [
+            {x:4013357.65,r:434/2,y:364}
+         ]
+       },
+       {
+        label: ["Call of Duty: World War II"],
+        backgroundColor: "rgba(183, 202, 125, 0.6)",
+        borderColor: "rgba(183, 202, 125, 1)",
+          data: [
+            {x:4276751.83,r:38/2,y:220}
+         ]
+       },
+       {
+        label: ["Arena of Valor"],
+        backgroundColor: "rgba(7, 178, 64, 0.6)",
+        borderColor: "rgba(7, 178, 64, 1)",
+          data: [
+            {x:3324141.25,r:18/2,y:210}
+         ]
+       },
+       {
+        label: ["Rocket League"],
+        backgroundColor: "rgba(163, 53, 72, 0.6)",
+        borderColor: "rgba(163, 53, 72, 1)",
+          data: [
+            {x:2297363.32,r:74/2,y:287}
+         ]
+       },
+        ]
     };
   }
 
@@ -26,6 +122,72 @@ class Prize extends React.Component {
       <Bubble
         data={{
           datasets: this.state.datasets
+        }}
+        options={{
+          legend: {
+            display: true
+          },
+          pan: {
+            enabled: true, // Enable panning
+            mode: "x", // Allow panning in the x direction
+            rangeMin: {
+              x: null // Min value of the delay option
+            },
+            rangeMax: {
+              x: null // Max value of the delay option
+            }
+          },
+          zoom: {
+            enabled: true, // Enable zooming
+            mode: "x", // Allow zooming in the x direction
+            rangeMin: {
+              x: null // Min value of the duration option
+            },
+            rangeMax: {
+              x: null // Max value of the duration option
+            }
+          },
+          responsive: true,
+          maintainAspectRatio: true,
+          plugins: {
+            streaming: {
+              frameRate: 120
+            }
+          },
+          scales: {
+            yAxes:[{
+                    stacked:true,
+                gridLines: {
+                    display:true,
+                },
+                ticks: {
+                   suggestedMax: 100,
+                   suggestedMin: -10000
+                }
+            }],
+            xAxes:[{
+                    gridLines: {
+                    display:false
+                }
+            }]
+          },
+          tooltips: {
+            callbacks: {
+              // this callback is used to create the tooltip label
+              label: function(tooltipItem, data) {
+                // get the data label and data value to display
+                // convert the data value to local string so it uses a comma seperated number
+                var dataLabel = data.datasets[tooltipItem.datasetIndex].label;
+                // add the currency symbol $ to the label
+                var revenue = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].y;
+                revenue = Math.trunc(revenue);
+                var value = ': ' + commaFormat(revenue);
+                dataLabel += value;
+                // return the text to display on the tooltip
+                return dataLabel;
+              }
+            }
+          }
         }}
       />
       </div>
